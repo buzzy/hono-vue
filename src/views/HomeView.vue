786c99285bdb48dfa0546ddebd1367f9a87cf0d1
Daniele Testa <daniele.testa@gmail.com>
2024-07-29 22:36:57 +0200

    Initial version

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { AppType } from '../api/server'
+import { hc } from 'hono/client'
+
+const handleClick = async () => {
+  console.log('Button clicked')
+
+  const client = hc<AppType>('/api')
+}
+</script>
+
+<template>
+  <main>
+    <button @click="handleClick">Click Me</button>
+  </main>
+</template>
